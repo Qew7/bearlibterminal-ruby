d29af233250db953f95858a97dc5ab8e2a6bc39b
Qew7 <qewwork@rambler.ru>
2018-07-11 13:38:22 +0300

    first commit

---
+++ config.rb
@@ -0,0 +1,5 @@
+class Config
+  TITLE = 'Hello'
+  WINDOW_WIDGHT = 800
+  WINDOW_HEIGHT = 600
+end
---
+++ main.rb
@@ -0,0 +1,19 @@
+require_relative './lib/BearLibTerminal'
+require_relative 'config'
+
+class Main
+  def perform
+    setup
+  end
+
+  private
+
+  def setup
+    Terminal.open
+    Terminal.set("window.title = '#{Config::TITLE}'")
+    Terminal.set("font: assets/Fix15Mono-Bold.ttf, size=14x14")
+    Terminal.set("window.size = #{Config::WINDOW_WIDGHT}x#{Config::WINDOW_HEIGHT}")
+  end
+end
+
+Main.new.perform
